c0ad31f67f78c8255a544e55d8f282da75024bb5
Sn0w <andreitaran@outlook.com>
2024-06-17 00:50:46 +0100

    > - null checks

--- src/main/java/ImGui/VersionManager.java
+++ src/main/java/ImGui/VersionManager.java
@@ -26,7 +26,7 @@ public class VersionManager {
             }
         }
 
-        String version = versionProps.getProperty(VERSION_KEY, "1.2");
+        String version = versionProps.getProperty(VERSION_KEY, "1.3");
         return version;
     }
     public static void displayVersion(float windowWidth) {
