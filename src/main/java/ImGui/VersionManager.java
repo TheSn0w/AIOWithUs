e9ff128b097da3fc5288ee4cc9ac3b81b54eee70
Sn0w <andreitaran@outlook.com>
2024-06-29 21:27:27 +0100

    > - Small fixes

--- src/main/java/ImGui/VersionManager.java
+++ src/main/java/ImGui/VersionManager.java
@@ -26,7 +26,7 @@ public class VersionManager {
             }
         }
 
-        String version = versionProps.getProperty(VERSION_KEY, "1.6");
+        String version = versionProps.getProperty(VERSION_KEY, "1.7");
         return version;
     }
     public static void displayVersion(float windowWidth) {
