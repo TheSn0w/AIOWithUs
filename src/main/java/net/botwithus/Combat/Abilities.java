9b2396541241ee64c267306ec549e21dcffa55fd
Sn0w <andreitaran@outlook.com>
2024-06-30 01:18:53 +0100

    > - Small fixes

--- src/main/java/net/botwithus/Combat/Abilities.java
+++ src/main/java/net/botwithus/Combat/Abilities.java
@@ -35,7 +35,7 @@ public class Abilities {
         int currentNecrosisStacks = VarManager.getVarValue(VarDomainType.PLAYER, 10986);
 
         long currentTime = System.currentTimeMillis();
-        if (currentTime - lastAbilityTime < 1500) {
+        if (currentTime - lastAbilityTime < random.nextLong(1800, 1850)) {
             return;
         }
 
